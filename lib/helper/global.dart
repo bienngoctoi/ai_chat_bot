9fb1b455a497ad9a7a844669384f550af1ae2695
Ngoc Toi <bienngoctoi@gmail.com>
2024-07-24 01:20:12 +0700

    fix push

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -1,8 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 const appName = "AI Assistant";
 late Size mq;
 
-//ChatGPT api key
-// const apiKey = "sk-None-gVQpH6F5F7qFq6wfUchOT3BlbkFJezgjiMVBtDXHpaSEGxvg";
-const apiKey = "";
+String apiKey = Platform.environment['sk-None-gVQpH6F5F7qFq6wfUchOT3BlbkFJezgjiMVBtDXHpaSEGxvg'] ?? '';
+
